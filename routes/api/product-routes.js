5110c82adbb57757f468fcfcca290fbcda572f4d
nqg <nidaghuman@gmail.com>
2021-03-23 09:48:26 -0400

    fixed typo

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
     res.status(200).json(productData);
 
   } catch (err) {
-    res.status(500).json(err):
+    res.status(500).json(err)
   }
 });
 
@@ -128,7 +128,7 @@ router.delete('/:id', async (req, res) => {
    }
 
    res.status(200).json(productData);
-   
+
   } catch (err) {
     res.status(500).json(err);
   }
